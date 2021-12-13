ea1f3d840b64093b40ebba0e3dfceaacd4b36716
Chris S <chris@soundshare.co.uk>
2021-12-13 16:17:54 +0000

    wip

--- default.nix
+++ default.nix
@@ -1,13 +1,23 @@
 { sources ? import ./nix/sources.nix
+, compiler ? "ghc865"
 } :
 let
   niv = import sources.nixpkgs {
     overlays = [
       (_ : _ : { niv = import sources.niv {}; })
     ] ;
-    config = {};
+    config = {
+      allowBroken = true;
+    };
   };
   pkgs = niv.pkgs;
-  myHaskellPackages = pkgs.haskellPackages;
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+      webdriver-w3c = pkgs.haskell.lib.dontCheck (self.callCabal2nix "webdriver-w3c" (builtins.fetchTarball {
+        url = "https://hackage.haskell.org/package/webdriver-w3c-0.0.2/webdriver-w3c-0.0.2.tar.gz";
+      }) {});
+      script-monad = pkgs.haskell.lib.dontCheck (super.script-monad);
+    };
+  };
 in
 myHaskellPackages.callCabal2nix "HaskellNixCabalStarter" (./.) {}
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Api.WebDriver.Endpoints
+import Web.Api.WebDriver
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  x <- execWebDriverT defaultWebDriverConfig doLogin
+  print x
+
+doLogin :: WebDriverT IO ()
+doLogin = do
+  navigateTo "http://localhost:8001/login"
+  z <- findElements CssSelector "div.alert"
+  assertEqual [] z "Errors found"
+
